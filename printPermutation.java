b6fcd6108607ea40981d73e6b20fde66d7875a75
NimitSharma23 <83410629+NimitSharma23@users.noreply.github.com>
2022-12-30 10:28:01 +0530

    Add files via upload

---
+++ printPermutation.java
@@ -0,0 +1,20 @@
+public class printPermutation {
+    public static void printperm(String str, String permutation){
+
+        if (str.length()==0){
+            System.out.println(permutation);
+            return;
+        }
+
+        for(int i = 0;i<str.length();i++){
+            char currChar = str.charAt(i);
+            String newString = str.substring(0,i)+str.substring(i+1);
+            printperm(newString,permutation+currChar);
+        }
+
+    }
+    public static void main(String[] args) {
+        String str = "abc";
+        printperm(str,"");
+    }
+}
